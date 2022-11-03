3f9de7fbeb410d059460b8ef52d19a2f1eb39ccf
Scott A. Green <sagreenxyz@gmail.com>
2022-11-03 15:42:08 -0500

    helper function to DRY setAttributes

--- script.js
+++ script.js
@@ -9,17 +9,28 @@ const apiKey = 'Y3rNCqlocerYqeHBI9G4rphMDAoCYVSAKo_BZ0v22gw'
 // const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 const apiUrl = './sample-data.json'
 
+// Helper function to set DOM element attributes
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
+
 // Create elements for links and photos, add to DOM
 function displayPhotos() {
     photosArray.forEach(photo => {
         const item = document.createElement('a')
-        item.setAttribute('href', photo.links.html)
-        item.setAttribute('target', '_blank')
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        })
 
         const img = document.createElement('img')
-        img.setAttribute('src', photo.urls.regular)
-        img.setAttribute('alt', photo.alt_description)
-        img.setAttribute('title', photo.alt_description)
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
 
         item.appendChild(img)
         imageContainer.appendChild(item)
